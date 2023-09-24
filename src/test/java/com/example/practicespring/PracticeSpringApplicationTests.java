c1a128484579b3cc3d516839319fd2b8da025ec0
LeeJungHoon <gns14585@naver.com>
2023-09-24 17:55:49 +0900

    Pratice

---
+++ src/test/java/com/example/practicespring/PracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
